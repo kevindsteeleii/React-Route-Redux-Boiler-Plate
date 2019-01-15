2065059ce7263ac816bcec70a7baa3fc187f7729
github username <github email address>
2019-01-15 18:39:59 -0500

    checking update after rename

--- app/index.js
+++ app/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './src/scss/index.scss';
+// comment used to check the rename
 
 ReactDOM.render(<App/>, document.getElementById('app'));
